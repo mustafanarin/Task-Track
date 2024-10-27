fa0452714e8ec5c641dd742cf92d0fd03fa3e265
Mustafa Narin <mustafa.narin1132@gmail.com>
2024-10-27 13:26:03 +0300

    android gradle settings updated

--- lib/product/utility/initialize/application_start.dart
+++ lib/product/utility/initialize/application_start.dart
@@ -1,16 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:todo_app/firebase_options.dart';
+import 'package:todo_app/firebase_options.dart'; 
 
 @immutable
 class ApplicationStart {
   const ApplicationStart._();
-  
+
   static Future<void> init() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
-    await _setPreferredOrientations();
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.android,
+      );
+
+      // Set device orientation preferences
+      await _setPreferredOrientations();
+    } catch (e) {
+      debugPrint('Initialization Error: $e');
+      rethrow;
+    }
   }
 
   static Future<void> _setPreferredOrientations() async {
